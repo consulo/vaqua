07706e3bff50cd7c35e3205c40150c0683338b60
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-07-08 23:49:24 +0300

    added java8-java9 classes to vaqua
    add shade module for vaqua

---
+++ vaqua/java8-impl/src/main/java/org/violetlib/aqua/Aqua8PopupFactory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015-2018 Alan Snyder.
+ * All rights reserved.
+ *
+ * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
+ * accompanying license terms.
+ */
+
+package org.violetlib.aqua;
+
+import java.awt.*;
+import java.lang.reflect.Method;
+import javax.swing.*;
+
+/**
+ * A Java 8 specific popup factory.
+ */
+
+public class Aqua8PopupFactory extends AquaPopupFactory {
+
+    @Override
+    protected Popup getHeavyweightPopup(Component owner, Component contents, int x, int y) {
+        // Heavy weight popups are required to support vibrant backgrounds and rounded corners.
+        // If we cannot configure this factory to create heavy weight popups, we will create a new popup directly.
+        try {
+            Method m = PopupFactory.class.getDeclaredMethod("setPopupType", Integer.TYPE);
+            m.setAccessible(true);
+            m.invoke(this, 2);
+        } catch (Throwable e) {
+            System.err.println("Unable to setPopupType: " + e);
+            return new AquaPopup(owner, contents, x, y);
+        }
+
+        Popup p = getDefaultPopup(owner, contents, x, y);
+
+        // Reusing popups is not working reliably. Not sure if there is a general timing problem or a change in
+        // behavior in El Capitan. The problem is that the stale contents are briefly displayed.
+        // See bug JDK-8040630.
+
+        try {
+            Method m = p.getClass().getDeclaredMethod("setCacheEnabled", Boolean.TYPE);
+            m.setAccessible(true);
+            m.invoke(p, false);
+        } catch (Exception ex) {
+            System.err.println("Unable to prevent popup from being reused");
+        }
+
+        return p;
+    }
+}
---
+++ vaqua/java9-impl/src/main/java/org/violetlib/aqua/Aqua9PopupFactory.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015-2018 Alan Snyder.
+ * All rights reserved.
+ *
+ * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
+ * accompanying license terms.
+ */
+
+package org.violetlib.aqua;
+
+import java.awt.Component;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.swing.Popup;
+import javax.swing.PopupFactory;
+
+/**
+ * A popup factory for Java 9 and later.
+ */
+
+public class Aqua9PopupFactory extends AquaPopupFactory
+{
+	private static final Method ourGetPopupMethod;
+
+	static
+	{
+		try
+		{
+			ourGetPopupMethod = PopupFactory.class.getMethod("getPopup", Component.class, Component.class, int.class, int.class, boolean.class);
+		}
+		catch(NoSuchMethodException e)
+		{
+			throw new Error(e);
+		}
+	}
+
+	@Override
+	protected Popup getHeavyweightPopup(Component owner, Component contents, int x, int y)
+	{
+		// Heavy weight popups are required to support vibrant backgrounds and rounded corners.
+		Popup p = null;
+		try
+		{
+			p = (Popup) ourGetPopupMethod.invoke(this, owner, contents, x, y, true);
+		}
+		catch(IllegalAccessException | InvocationTargetException e)
+		{
+			throw new Error(e);
+		}
+
+		// Reusing popups is not working reliably. Not sure if there is a general timing problem or a change in
+		// behavior in El Capitan. The problem is that the stale contents are briefly displayed.
+		// See bug JDK-8040630.
+
+		try
+		{
+			AquaUtils.disablePopupCache(p);
+		}
+		catch(Throwable th)
+		{
+			System.err.println("Unable to prevent popup from being reused");
+		}
+
+		return p;
+	}
+}
